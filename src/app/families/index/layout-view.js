46b0cca350f7aa030fb8f46f600055ed00ba5583
Iva Kop <iva@penguin.digital>
2018-07-25 13:29:37 +0300

    Show data initially instead of after search

--- src/app/families/index/layout-view.js
+++ src/app/families/index/layout-view.js
@@ -6,7 +6,7 @@ import CollectionView from './collection-view';
 import utils from '../../utils';
 import FamiliesCollection from '../collection';
 import OrganizationsModel from '../../management/organizations/model';
-import env from "../../env";
+import env from '../../env';
 
 export default Mn.View.extend({
   template: Template,
@@ -24,16 +24,27 @@ export default Mn.View.extend({
     this.app = options.app;
   },
   onRender() {
+    this.fetchHouseHolds();
     setTimeout(() => {
       this.$el.find('#search').focus();
     }, 0);
     this.showList();
-
     this.loadSelects();
+  },
+  fetchHouseHolds(params) {
+    const elements = new FamiliesCollection();
+    const self = this;
+    elements.fetch({
+      data: params,
+      success(response) {
+        self.collection = response;
+        self.showList();
+      }
+    });
   },
   showList() {
     this.getRegion('list').show(
-      new CollectionView({collection: this.collection})
+      new CollectionView({ collection: this.collection })
     );
   },
   loadSelects() {
@@ -67,16 +78,7 @@ export default Mn.View.extend({
         organization_id: $('#organization').val(),
         free_text: $('#search').val()
       };
-
-      const elements = new FamiliesCollection();
-      elements.fetch({
-        data: params,
-        success(response) {
-          self.collection = response;
-          self.showList();
-          section.reset();
-        }
-      });
+      self.fetchHouseHolds(params);
     }
   }
 });
