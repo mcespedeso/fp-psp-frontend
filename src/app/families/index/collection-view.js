f51d4528cfe60f27d7af4fc23da1e6c7f24c885d
Iva Kop <iva@penguin.digital>
2018-07-25 13:41:42 +0300

    Display households as a list

--- src/app/families/index/collection-view.js
+++ src/app/families/index/collection-view.js
@@ -3,8 +3,6 @@ import ItemView from './item/view';
 
 export default Mn.CollectionView.extend({
   childView: ItemView,
-  childViewOptions: {
-    className: 'col-lg-2 col-md-4 col-sm-4 col-xs-12'
-  },
+  childViewOptions: {},
   className: 'list-container row'
 });
